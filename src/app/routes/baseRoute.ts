4dee289b0ac7e4cf05d3900e3e258281dc7cca60
Rodrigo Walace Silva <rodrigo.walace.silva@gmail.com>
2025-03-03 19:45:38 -0300

    feat: criando set customizado para adicionar novas rotas

---
+++ src/app/routes/baseRoute.ts
@@ -0,0 +1,31 @@
+import type { RouteOptions } from "fastify";
+
+class BaseRouteSet<
+  R extends RouteOptions = RouteOptions,
+  P extends string = string,
+> extends Set<R> {
+  /**
+   * Must contain only the prefix name; no `/`'s
+   * Okay: "user"
+   * NOT Okay: "/user", "/user/", "user/"
+   */
+  #prefix: string;
+
+  constructor(prefix: P extends `/${string}` ? never : P) {
+    super();
+
+    this.#prefix = prefix;
+  }
+
+  override add(value: R & { url: `/${string}` | "" }): this {
+    // super.add()
+    super.add({
+      ...value,
+      url: this.#prefix ? `/${this.#prefix}${value.url}` : value.url,
+    });
+
+    return this;
+  }
+}
+
+export { BaseRouteSet };
