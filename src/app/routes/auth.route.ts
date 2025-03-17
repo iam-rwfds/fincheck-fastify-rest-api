7cf6686f7d71bf485a2c49fb41573db7eab3e63f
Rodrigo Walace Silva <rodrigo.walace.silva@gmail.com>
2025-03-16 21:50:21 -0300

    refactor: chamando provider de autenticação nas rotas

--- src/app/routes/auth.route.ts
+++ src/app/routes/auth.route.ts
@@ -1,16 +1,25 @@
-import type { RouteOptions } from "fastify";
+import { container } from "../../infra/container";
+import type { AuthProvider } from "../controllers/AuthProvider";
 import { BaseRouteSet } from "./baseRoute";
 
 const authRoutes = new BaseRouteSet("auth");
 
 authRoutes.add({
-  handler: () => {},
+  handler: (req, reply) => {
+    const authProvider = container.resolve<AuthProvider>("authProvider");
+
+    authProvider.signin(req, reply);
+  },
   method: "POST",
   url: "/signin",
 });
 
 authRoutes.add({
-  handler: () => {},
+  handler: (req, reply) => {
+    const authProvider = container.resolve<AuthProvider>("authProvider");
+
+    authProvider.signup(req, reply);
+  },
   method: "POST",
   url: "/signup",
 });
