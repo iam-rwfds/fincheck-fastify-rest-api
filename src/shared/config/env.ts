e846635a975a4af677a523f629bbb13674010b85
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-03-28 15:59:43 -0300

    refactor: adicionando variaveis de ambiente do appwrite no objeto de variaveis de ambiente principal

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,61 +1,51 @@
-import * as v from 'valibot'
+import * as v from "valibot";
+import { type IAppWriteEnv, appWriteEnv } from "./appwrite.env";
 
 interface IEnv {
-  // get apiKey(): string;
   get host(): string;
   get port(): number;
+  get appWrite(): IAppWriteEnv | null;
 }
 
 class Env implements IEnv {
-  // #apiKey: string
-  #host: string;
-  #port: number;
+  #host: IEnv["host"];
+  #port: IEnv["port"];
+  #appWrite: IEnv["appWrite"];
 
   constructor(init: IEnv) {
-    // this.#apiKey = init.apiKey
-    this.#host = init.host
-    this.#port = init.port
-  }  
-  
-  // get apiKey() {
-    // return this.#apiKey;
-  // }
+    this.#host = init.host;
+    this.#port = init.port;
+    this.#appWrite = init.appWrite || null;
+  }
 
   get host() {
-    return this.#host
+    return this.#host;
   }
 
   get port() {
-    return this.#port
+    return this.#port;
+  }
+
+  get appWrite(): IAppWriteEnv | null {
+    return this.#appWrite;
   }
 }
 
 const env = new Env({
-  // apiKey: process.env.API_KEY,
   port: Number(process.env.PORT),
-  host: process.env.HOST
-} as IEnv)
+  host: process.env.HOST,
+  appWrite: appWriteEnv || null,
+} as IEnv);
 
 const EnvSchema = v.object({
-  // apiKey: v.pipe(
-  //   v.string(),
-  //   v.nonEmpty()
-  // ),
-  port: v.pipe(
-    v.number(),
-    v.integer(),
-    v.minValue(1)
-  ),
-  host: v.pipe(
-    v.string(),
-    v.nonEmpty()
-  )
-})
+  port: v.pipe(v.number(), v.integer(), v.minValue(1)),
+  host: v.pipe(v.string(), v.nonEmpty()),
+});
 
-const parsedSchema = v.safeParse(EnvSchema, env)
+const parsedSchema = v.safeParse(EnvSchema, env);
 
 if (parsedSchema.issues) {
-  throw new Error(JSON.stringify(parsedSchema.issues, null, 2))
+  throw new Error(JSON.stringify(parsedSchema.issues, null, 2));
 }
 
-export { env }
+export { env };
