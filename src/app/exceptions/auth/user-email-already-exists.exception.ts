5dd9fb3a11a4feb7147549ff910d8e62d83dba68
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-03-28 18:17:02 -0300

    feat: criando classe de exceção para quando um usuário se cadastro com o mesmo email de outro que já existe

---
+++ src/app/exceptions/auth/user-email-already-exists.exception.ts
@@ -0,0 +1,12 @@
+import { EmptyException } from "~infra/exception";
+
+class UserEmailAlreadyExistsException extends EmptyException {
+    constructor() {
+        super({
+            statusCode: 409,
+            message: "Um usuário com este email já está cadastrado."
+        })
+    }
+}
+
+export { UserEmailAlreadyExistsException };
