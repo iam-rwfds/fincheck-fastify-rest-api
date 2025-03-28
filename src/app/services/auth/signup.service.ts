05fdb5226db72b5c232d66f558362b314aeb1e85
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-03-28 18:18:39 -0300

    feat: declarando esqueleto de serviço de cadastro

---
+++ src/app/services/auth/signup.service.ts
@@ -0,0 +1,23 @@
+import * as awilix from "awilix";
+import { either, type Either } from "~utils/either";
+import { container } from "~infra/container";
+import type { UserEmailAlreadyExistsException } from "~exceptions/auth/user-email-already-exists.exception";
+
+type Response = Either<
+  UserEmailAlreadyExistsException,
+  {
+    accessToken: string;
+  }
+>;
+
+class AuthSignUpService {
+  async execute(): Promise<Response> {
+    return either.right({
+      accessToken: "",
+    });
+  }
+}
+
+container.register({
+  authSignUpService: awilix.asClass(AuthSignUpService),
+})
