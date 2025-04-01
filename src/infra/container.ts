43575b4b16bd8e43eafd6d75b50ab0dd32745c9f
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-04-01 18:22:15 -0300

    refactor: registrando serviço de login no container com seu respectivo token

--- src/infra/container.ts
+++ src/infra/container.ts
@@ -4,6 +4,7 @@ import { databases } from "~database/databaseClient";
 import { UsersRepository } from "../app/repositories/users.repository";
 import { AuthSignUpService } from "../app/services/auth/signup.service";
 import { TOKENS } from "./tokens";
+import { AuthSignInService } from "../app/services/auth/signin.service";
 
 export const container = awilix.createContainer({
   injectionMode: awilix.InjectionMode.PROXY,
@@ -14,6 +15,7 @@ container.register({
   [TOKENS.Database]: awilix.asValue(databases),
 
   [TOKENS.Auth.Services.SignUp]: awilix.asClass(AuthSignUpService),
+  [TOKENS.Auth.Services.SignIn]: awilix.asClass(AuthSignInService),
   [TOKENS.Auth.Provider]: awilix.asClass(AuthProvider),
 
   [TOKENS.Users.Repository]: awilix.asClass(UsersRepository),
