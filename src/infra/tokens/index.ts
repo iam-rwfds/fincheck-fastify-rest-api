032bcb7a18230db73e131b9ef73c0484312b8c5b
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-04-07 15:17:02 -0300

    refactor: adicionando tokens associados às categorias ao objeto de token principal

--- src/infra/tokens/index.ts
+++ src/infra/tokens/index.ts
@@ -1,10 +1,12 @@
 import { authTokens } from "./auth.tokens";
+import { categoriesTokens } from "./categories.tokens";
 import { usersTokens } from "./users.tokens";
 
 const TOKENS = Object.freeze({
   Auth: authTokens,
   Database: Symbol.for("Database"),
   Users: usersTokens,
+  Categories: categoriesTokens,
 });
 
 export { TOKENS };
