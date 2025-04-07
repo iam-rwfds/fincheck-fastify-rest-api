004ae9a83c759670471e4e3291d4a7e19de6f04a
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-04-06 21:19:54 -0300

    feat: criando tokens pro serviço "me" de usuário
    Este serviço retorna os dados do usuário logado

--- src/infra/tokens/users.tokens.ts
+++ src/infra/tokens/users.tokens.ts
@@ -1,5 +1,9 @@
 const usersTokens = Object.freeze({
   Repository: Symbol("Repository"),
+  Controller: Symbol("Controller"),
+  Services: {
+    Me: Symbol.for("ME"),
+  },
 });
 
 export { usersTokens };
