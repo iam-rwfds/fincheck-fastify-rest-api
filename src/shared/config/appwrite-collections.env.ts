8105d621862661df97b3ae9e3d28ac31ee0db0da
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-04-28 18:23:00 -0300

    feat: adicionando validação pra coleção de contas bancárias

--- src/shared/config/appwrite-collections.env.ts
+++ src/shared/config/appwrite-collections.env.ts
@@ -4,15 +4,18 @@ import * as v from "valibot";
 interface IEnv {
   get usersId(): string;
   get categoriesId(): string;
+  get bankAccountsId(): string;
 }
 
 class AppWriteCollectionsEnv implements IEnv {
   #usersId: string;
   #categoriesId: string;
+  #bankAccountsId: string;
 
   constructor(init: IEnv) {
     this.#usersId = init.usersId;
     this.#categoriesId = init.categoriesId;
+    this.#bankAccountsId = init.bankAccountsId;
   }
 
   get usersId(): string {
@@ -22,16 +25,22 @@ class AppWriteCollectionsEnv implements IEnv {
   get categoriesId(): string {
     return this.#categoriesId;
   }
+
+  get bankAccountsId(): string {
+    return this.#bankAccountsId;
+  }
 }
 
 const collectionsEnv = new AppWriteCollectionsEnv({
   usersId: env.APPWRITE_COLLECTIONS_USER_ID ?? "",
   categoriesId: env.APPWRITE_COLLECTIONS_CATEGORIES_ID ?? "",
+  bankAccountsId: env.APPWRITE_COLLECTIONS_BANK_ACCOUNTS_ID ?? "",
 });
 
 const EnvSchema = v.object({
   usersId: v.pipe(v.string(), v.nonEmpty()),
   categoriesId: v.pipe(v.string(), v.nonEmpty()),
+  bankAccountsId: v.pipe(v.string(), v.nonEmpty()),
 });
 
 const parsedSchema = v.safeParse(EnvSchema, collectionsEnv);
