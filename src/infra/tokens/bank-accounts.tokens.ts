fa8ddfaa8b1802d5a38d7af3e56c394e6737d9b1
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-05-28 16:22:49 -0300

    feat(bank-accounts): Adicionar token para serviço de obtenção de contas

--- src/infra/tokens/bank-accounts.tokens.ts
+++ src/infra/tokens/bank-accounts.tokens.ts
@@ -2,6 +2,7 @@ const services = Object.freeze({
   Create: Symbol("CreateBankAccountService"),
   AssertUserRelation: Symbol("AssertUserRelationService"),
   Update: Symbol("UpdateBankAccountService"),
+  GetAllFromUser: Symbol("GetAllBankAccountsFromUserService"),
 });
 
 const bankAccountsTokens = Object.freeze({
