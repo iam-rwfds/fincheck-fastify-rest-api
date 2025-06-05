8112b938079c6151f2a452b7c2b79a71703f562f
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-06-05 09:50:46 -0300

    feat(transactions): Implementa serviço de criação de transação

--- src/app/services/transactions/create.service.ts
+++ src/app/services/transactions/create.service.ts
@@ -1,5 +1,6 @@
 import { TOKENS } from "~infra/tokens";
 import type { TransactionsRepository } from "~repositories/transactions.repository";
+import type { Transaction } from "../../entities/transaction.entity";
 
 type IServiceConstructorParams = {
   [key in symbol]: TransactionsRepository;
@@ -15,4 +16,22 @@ abstract class AbstractService {
   get bankAccountsRepository() {
     return this.#bankAccountsRepository;
   }
+
+  abstract execute(
+    dto: Pick<Transaction, "date" | "name" | "type" | "value"> & {
+      [key in `${"user" | "bankAccount" | "category"}Id`]: string;
+    },
+  ): Promise<Transaction>;
 }
+
+class Service extends AbstractService {
+  async execute(
+    dto: Pick<Transaction, "date" | "name" | "type" | "value"> & {
+      [key in `${"user" | "bankAccount" | "category"}Id`]: string;
+    },
+  ): Promise<Transaction> {
+    return await this.bankAccountsRepository.create(dto);
+  }
+}
+
+export { Service as CreateTransactionService };
