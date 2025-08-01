d184e1abaacb41b0f55123ad64a2a6f73edafbca
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-08-01 11:29:29 -0300

    feat(categories): Implementa endpoint /mine de categorias com autenticação

--- src/app/routes/categories.route.ts
+++ src/app/routes/categories.route.ts
@@ -1,15 +1,19 @@
+import { container } from "~infra/container";
 import { BaseRouteSet } from "./baseRoute";
+import { TOKENS } from "~infra/tokens";
+import type { CategoriesController } from "~controllers/categories.controller";
 
 const categoriesRoutes = new BaseRouteSet("categories");
 
 categoriesRoutes.add({
-  handler: () => {
-    return {
-      categories: [],
-    };
+  handler: (req, reply) => {
+    const categoriesControllers = container.resolve<CategoriesController>(TOKENS.Categories.Controller);
+
+    return categoriesControllers.show(req, reply);
   },
   method: "GET",
   url: "/mine",
+  onRequest: (req, reply) => req.server.authenticate(req, reply),
 });
 
 export { categoriesRoutes };
