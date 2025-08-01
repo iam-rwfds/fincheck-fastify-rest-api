8c04c849dceeff768a54015e34749653b7aa81ea
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-08-01 11:28:54 -0300

    refactor(categories): adicionar tokens para DI de serviços e controller

--- src/infra/tokens/categories.tokens.ts
+++ src/infra/tokens/categories.tokens.ts
@@ -1,5 +1,9 @@
 const categoriesTokens = Object.freeze({
   Repository: Symbol("Repository"),
+  Services: Object.freeze({
+    Mine: Symbol.for("CategoriesMineService")
+  }),
+  Controller: Symbol.for("CategoriesController")
 });
 
 export { categoriesTokens };
